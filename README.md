0d2694aa636414495bb54508a68ca3ad1a384e59
jmuriungi18 <jmuriungi18@my.whitworth.edu>
2022-04-06 12:28:45 -0700

    more work on RR scheduler function, there are some parts I'm not sure of, I have commented my thought process

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CPU Scheduler Dispatch Sim
 
 ## Assumptions
-
+Processes are assigned to cpu objects upon creation of a cpu
 ## Design
 
 ## 
--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,23 @@
+//#include "../headers/cpu.h"
+#include <iostream>
+#include <ios>
+#include <string>
+#include <istream>
 #include <pthread.h>
 #include <vector>
 #include <algorithm>
-#include <string> 
-
-#include "../headers/cpu.h"
-
-// using namespace std;
+#include <map>
+#include <ctime>
+#include <fstream>
+#include <ostream>
+#include <deque>
 
 using std::vector;
 using std::fstream;
-
-#include <fstream>
-#include <iostream>
+using std::map;
+using std::string;
+using std::ios;
+using std::cout;
 
 #define CPU_COUNT 5
 // mutex <- for interaction somewhere
@@ -28,7 +34,7 @@ int PID_ASSIGNED_NUM = 0;
 
 struct process {
     int pid;
-    std::string data = ""; // Data generated for the process to hold 
+    string data = ""; // Data generated for the process to hold 
 };
 
 struct pcb {
@@ -125,13 +131,13 @@ class cpu {
         // For each process and pcb, do the following
         /* - Create a Fstream and create a new file by name of CPU#_PROC# such as "cpu1proc1" or something */
         fstream cpu_data;
-        cpu_data.open("data_file.csv", std::ios::in);
+        cpu_data.open("data_file.csv", std::fstream::in);
         //Does the file exist?
         if (cpu_data.fail()) { std::cout << "The file you are trying to open DNE.\n"; exit(1); } //Verify file actually opened
 
         cpu_data << "PID # | ARRIVAL TIME | CPU TIME | IO TIME | (Repeating on until process ends)\n"; // Write the code for the values being placed into the table
         // Possibly comment out the line above to make code easier, and have this here instead as a key to refer too.
-        
+
         for(int i=0; i<processCount; i++) {
             processes[i].pid = PID_ASSIGNED_NUM; // Set the PID of the processes, starting at 1
             PID_ASSIGNED_NUM++;
@@ -140,7 +146,7 @@ class cpu {
             pcbs[i].arrivalTime = 0;    // Set arrivalTime to 0 initially
             pcbs[i].ioTime = 0; // Set ioTime to 0 initially
             pcbs[i].priority = priority; // Set the priority given, 0 be default | low, medium, high, critical
-            pcbs[i].state = true; // Set the state to ready
+            pcbs[i].state = false; // Set the state to waiting
             pcbs[i].cpuBurstTime = 0; // Set the cpu burst time to 0
             pcbs[i].accumulated_T = 0; // set the accumulated_T to 0 due to being just created
             pcbs[i].accumulated_W = 0; // set the accumulated_T to 0 due to being just created
@@ -148,7 +154,7 @@ class cpu {
             // Write to the cpu's file for this process on line [i]
             //- Form a string with the data just made above, then place that string in the CSV file on the line [i]+1 (or 2?)
             // PID #, Arrival t, CPU t, IO t, CPU t, IO t, CPU t 
-            cpu_data <<pcbs[i].pid <<","<< pcbs[i].arrivalTime <<","<< pcbs[i].cpuBurstTime <<","<< pcbs[i].ioTime;
+            // cpu_data <<pcbs[i].pid <<", "<< pcbs[i].arrivalTime <<", "<< pcbs[i].cpuBurstTime <<", "<< pcbs[i].ioTime <<", "<< pcbs[i].cpuBurstTime <<", "<< pcbs[i].ioTime;
         }
         // Close the fstream
         cpu_data.close(); 
@@ -166,12 +172,24 @@ class cpu {
     /* -- base class functions -- */
     int runproc(pcb* _selectedProcess, int runtime) // runs a process at specified 
     {   
+        
+        // update burst time, iotime, etc
         int temp_ioTime = rand() % 50;  // simulate random i/o time
         int temp_cpu_burst = rand() % 20;   // simulate random cpu time
+        //_selectedProcess->arrivalTime = clock or time of system?
+        //_selectedProcess->accumulated_W = time spent in wait queue
+        //_selectedProcess->accumulated_R = response time, not sure how to calculate
         _selectedProcess->ioTime = temp_ioTime; // assign simulated i/o time to selected process
         _selectedProcess->cpuBurstTime = temp_cpu_burst;    // assign simulated cpu time to selected process
         _selectedProcess->accumulated_T = (_selectedProcess->accumulated_W + runtime);  // turnaround time = wait time + execution time
         
+        // print process information to data file once process is run
+        fstream proc_data;
+        proc_data.open("data_file.csv", fstream::in);
+        //Does the file exist?
+        if (proc_data.fail()) { std::cout << "The file you are trying to open DNE.\n"; exit(1); } //Verify file actually opened
+        proc_data << _selectedProcess->pid <<", " << _selectedProcess->cpuBurstTime << ", " << _selectedProcess->ioTime << ", " << _selectedProcess->cpuBurstTime << ", " << _selectedProcess->ioTime << ", " << _selectedProcess->cpuBurstTime;
+        proc_data.close();
         /*
         process* p_pointer; // Points to the process it is containing the information about
         int pid; // pid = process.get_id(); Process ID (ID of the process)
@@ -185,11 +203,6 @@ class cpu {
         int accumulated_R; // response time
     */
         int success = 0;    // maybe should be bool?
-        // pick a random process from process table
-        // update burst time, iotime, etc
-        
-
-        // Update Information
 
         return success;
     }
@@ -211,16 +224,43 @@ class cpu {
     present.table.getPid(int index) <- Return the pointer to the pid at the index given in the vector of pid's
 */
 vector<int> FCFS(cpu present){
-
+    int curno = present.table.getPidList().size();
     vector<int> holder;
-    //grab processes within the present CPU
+
+    std::deque<pcb *> wait_queue;
+    std::deque<pcb *> ready_queue;
     
-    //set fixed cpu 
-    //until all processes are done
+    //grab processes within the present CPU
+    //put some in a waiting queue
+    for(int i=0; i<present.table.getPidList().size(); i++)
+    {
+        wait_queue.push_back(present.table.getProcessPcb(i));        
+    }
+
+    //while the wait queue still has processes
+    while(!wait_queue.empty())
+    {
+        //select a random number of processes from wait queue and add to ready queue
+        int randno = rand() % curno;
+        curno = curno -randno;
+        //move processes from wait queue to ready queue
+        for(int i =0; i <= randno; i++){
+            //get top process of wait queue because pop destroys element
+            pcb* curr_process = wait_queue.front();
+            //pop process from wait queue
+            wait_queue.pop_front();
+            //add to ready queue
+            ready_queue.push_front(curr_process);
+        }        
+    }
+    //run processes in the ready queue
+    std::deque<pcb*> ready_queue;
+    //add all processes of the cpu to the wait_queue
+
      
     for (int i = 0; i < present.table.getPidList().size(); i++){
         //run the next process
-        present.runproc(present.table.getProcessPcb(i));
+        present.runproc(present.table.getProcessPcb(i), present.table.getProcessPcb(i)->ioTime);
         //update process state
     }
  
@@ -235,47 +275,98 @@ return holder;
 
 }
 
+bool compare(std::pair<int ,int> i, std::pair<int, int> j) {
+  return i.second < j.second;
+}
+
 vector<int> SPT(cpu present){
 
      vector<int> holder;
-    
-      for (int i = 0; i < present.table.getPidList().size(); i++){
-          int x =0;
-        //find the minimum process time
-        //clear holder
-        holder.clear();
-        
-        for (int j =0; i <present.table.getPidList().size(); i++){
-        //fill holder with the process times
-        holder[i]= present.table.getProcessPcb(i).ioTime;
-        }
-        //get minimum element from holder
-        x = min_element(holder.begin(), holder.end());
-        //set that index to a large time than possible 
-        holder[x] = 1000;
+     map<int,int> processholder;
 
-        present.runproc(present.table.getProcessPcb(i));
-        //update process state
+     //fill map with iotimes and the index for the io time
+     for (int i =0; i <present.table.getPidList().size(); i++){
+         processholder.insert(std::pair<int, int>(i,present.table.getProcessPcb(i)->ioTime));
+     }
+
+     for(int i =0; i < present.table.getPidList().size(); i++){
+         std::pair<int, int> min = *min_element(processholder.begin(), processholder.end(), compare); 
+        //run process at the index for the minimum element
+        present.runproc(present.table.getProcessPcb(min.first), present.table.getProcessPcb(min.first)->ioTime);
+        //delete current min 
+        processholder.erase(min.second);
+
+
+     }
+     
         
         
+        return holder;
         
     }
-    //load processes into queue
-    //set fixed cpu
-    //until all processes are done
-    //use math min to get process with shortest processing 
-    //update the state to ready and run the process through cpus
+    
     //update times 
     //take process out of queue
     //get all the analytic information and store in holder
     
 
-return holder;
 
-}
 
-vector<int> Robin(){
+vector<int> Robin(cpu present){
     vector<int> holder;
+    // interrupt time should be 
+    int interrupt = rand() % 10;   // if process runtime = interrupt, stop running process
+    int curno = present.table.getPidList().size();
+    // holds processes just created
+    std::deque<pcb *> wait_queue;
+    // holds processes ready to be executed
+    std::deque<pcb *> ready_queue;
+    
+    //grab processes within the present CPU
+    //put some in a waiting queue
+    for(int i=0; i<curno; i++)
+    {
+        wait_queue.push_back(present.table.getProcessPcb(i));        
+    }
+    //while the wait queue still has processes
+    while(!wait_queue.empty())
+    {
+        // push top process in wait queue to ready queue to begin with
+        pcb* curr_process = wait_queue.front();
+        //add to ready queue
+        ready_queue.push_front(curr_process);
+        //pop process from wait queue
+        wait_queue.pop_front();
+        // while ready queue still has processes
+        while(!ready_queue.empty())
+        {
+            present.runproc(curr_process, curr_process->cpuBurstTime);
+            // interrupt the process somehow
+            if(curr_process->cpuBurstTime == interrupt)
+            {
+                // update process's cpu burst time
+                // move current process from ready queue to wait queue
+                pcb* interrupted_process = ready_queue.front();
+                wait_queue.push_back(interrupted_process);
+                ready_queue.pop_front();
+                // pick next process to run from ready queue on a FCFS basis
+                // interrupt it,
+                // move it to wait queue
+                // remove from ready queue
+                // pick next process to run from ready queue on a FCFS basis
+                // interrupt it,
+                // move it to wait queue
+                // remove from ready queue
+                // and on and on, until all processes executed, i.e wait queue empty? 
+                // how do we tell if a process has completed execution?
+            }
+        }
+
+        // put the process back to wait queue
+        // select next process to run from wait queue on a FCFS basis
+        // add it to ready queue and execute
+        // add interrupt time to
+    }
     //load processes into map with slot time
     //until all processes are done
     //run top process
@@ -293,6 +384,9 @@ int main () {
     // For each CPU created, assign this
 
     // Do the threading stuff here and queues and things
+    /* QUEUES*/
+    // Ready Queue
+    // Waiting Queue
 
     // from ready queue to execution
     // void select_process()
@@ -313,7 +407,7 @@ int main () {
     //
     //      }
     // }
-
+    
 
     return 0;
 }
